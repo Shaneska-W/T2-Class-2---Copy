6654ac5baa2c4962c217e6a973202599892e6ca0
Shaneska-W <241148@virtualwindow.co.za>
2024-04-29 18:03:24 +0200

    Task Two
    video 5-8

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+console.log("Hello world");
+console.log("I am " + 19 + " years old");
+console.log(5 + 7);
+console.log("5 + 7");
+
+const Pi= 3.14;
+let person = "John Doe";
+let dog= "Hunter", age = 15, subject ="development";
+
+function myFunction(){
+alert("How are you?");
+}
